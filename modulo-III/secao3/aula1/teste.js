7a9e34120b5c94dfe4ed8d5ca345f536dd143d56
Reginaldo Sena <regsena@gmail.com>
2022-06-13 00:21:01 -0400

    update

---
+++ modulo-III/secao3/aula1/teste.js
@@ -0,0 +1,27 @@
+/*
+ * Complete a função 'tripleTheChances' abaixo.
+ *
+ * O retorno da função é uma variável do tipo INTEGER_ARRAY.
+ * A função aceita o parâmetro chances do tipo INTEGER_ARRAY.
+ */
+
+/*
+ * Complete a função 'vezesLetraAparece' abaixo.
+ *
+ * É esperado que a função retorne um número inteiro.
+ * A função aceita os seguintes parâmetros:
+ *  1. frase (string)
+ *  2. letra (string)
+ */
+
+
+console.log("Reginaldo");
+var chances = [2];//,3,5,8,10];
+for(var i = 0; i < chances.legth; i++) {
+   // let aux;
+   // aux = chances[i];
+   // chances[i] = aux * 3;
+   chances[i] = 3;
+}
+
+console.log("Resultado: "+chances);
